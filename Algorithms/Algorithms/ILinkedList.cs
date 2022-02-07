5b783db67cfd33723e253a3c0e4d1d7bb44ee52e
Tanya <yallina.ta@yandex.ru>
2022-02-07 23:59:58 +1000

    Lesson2

---
+++ Algorithms/Algorithms/ILinkedList.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Algorithms
+{
+    public interface ILinkedList<T>
+    {
+        int GetCount(); 
+        void AddNode(T value);  
+        void AddNodeAfter(Node<T> node, T value); 
+        void RemoveNode(int index); 
+        void RemoveNode(Node<T> node);
+        Node<T> FindNode(T searchValue); 
+
+    }
+}
